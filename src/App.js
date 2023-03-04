5caf9d4bc24f8a81e17a0671336ccfd12ad98603
Abhishek Sharma <coder8248@gmail.com>
2023-03-04 11:32:34 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import InputShortner from './InputShortner';
 import './App.css';
+import { useState } from 'react';
 import LinkResult from './LinkResult';
 
 function App() {
+
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <div className="container">
-      <InputShortner />
-      <LinkResult />
+      <InputShortner setInputValue={setInputValue} />
+      <LinkResult inputValue={inputValue} />
     </div>
   );
 }
--- src/InputShortner.js
+++ src/InputShortner.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { useState } from 'react';
+function InputShortner({ setInputValue }) {
 
-function InputShortner() {
+    const [value, setValue] = useState("");
+
+    const handleClick = () => {
+        setInputValue(value);
+        setValue("");
+    }
     return (
         <div className='inputContainer'>
             <h1>URL <span>Shortner</span></h1>
             <div>
-                <input type="text" placeholder="Paste a link to shorten it" />
-                <button>Shorten</button>
+                <input
+                    type="text"
+                    placeholder="Paste a link to shorten it"
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                />
+                <button onClick={handleClick}>Shorten</button>
             </div>
         </div>
     );
